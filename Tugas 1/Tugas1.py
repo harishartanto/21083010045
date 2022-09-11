fc4ca248f3c1aad641dc9d49dc6fa45f8a4953c2
harishartanto <muhamadharishartanto@gmail.com>
2022-09-11 20:09:22 +0700

    Menyederhanakan skrip Tugas1.py & Tugas1.sh

--- Tugas 1/Tugas1.py
+++ Tugas 1/Tugas1.py
@@ -1,9 +1,11 @@
-print('-'*35)
+batas = '-' * 35
+
+print(batas)
 print('Tugas 1 SISOP Kelas-A')
-print('-'*35)
+print(batas)
 print('Nama: Muhamad Haris Hartanto')
 print('NPM : 21083010045')
-print('-'*35)
+print(batas)
 print('notes:')
-print('-'*35)
+print(batas)
 print('Halo dunia!\nIni adalah skrip Bash pertamaku di Linux')
--- Tugas 1/Tugas1.sh
+++ Tugas 1/Tugas1.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-printf '%.0s-' {1..35}
-echo -e  "\nTugas 1 SISOP Kelas-A"
-printf '%.0s-' {1..35}
-echo -e  "\nNama: Muhamad Haris Hartanto"
+
+batas=$(printf '%.0s-' {1..35})
+
+echo $batas
+echo "Tugas 1 SISOP Kelas-A"
+echo $batas
+echo "Nama: Muhamad Haris Hartanto"
 echo "NPM : 21083010045"
-printf '%.0s-' {1..35}
-echo -e  "\nnotes:"
-printf '%.0s-' {1..35}
-echo -e "\nHalo dunia!\nIni adalah skrip Bash pertamaku di Linux!"
+echo $batas
+echo "notes:"
+echo $batas
+echo -e  "Halo dunia!\nIni adalah skrip Bash pertamaku di Linux!"
