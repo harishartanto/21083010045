2ad92bd797c2aa03f5131621e17c30f3ba106f10
harishartanto <muhamadharishartanto@gmail.com>
2022-09-11 15:56:31 +0700

    Push Tugas1.py & Tugas1.sh

---
+++ Tugas 1/Tugas1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+printf '%.0s-' {1..35}
+echo -e  "\nTugas 1 SISOP Kelas-A"
+printf '%.0s-' {1..35}
+echo -e  "\nNama: Muhamad Haris Hartanto"
+echo "NPM : 21083010045"
+printf '%.0s-' {1..35}
+echo -e  "\nnotes:"
+printf '%.0s-' {1..35}
+echo -e "\nHalo dunia!\nIni adalah skrip Bash pertamaku di Linux!"
