f586ce1c862517dd54c97b2d34543a6adc45363f
harishartanto <muhamadharishartanto@gmail.com>
2022-10-12 14:47:26 +0700

    edit typo

--- Tugas 6/Array_MD.sh
+++ Tugas 6/Array_MD.sh
@@ -4,7 +4,7 @@
 #spasi (" ")sebagai pembagi baris dan titik dua (:) sebagai pembagi kolom 
 array2d="1.1:1.2:1.3:1.4 2.1:2.2:2.3:2.4 3.1:3.2:3.3:3.4"
 
-# Membuat function untuk membentuk array 2D
+#Membuat function untuk membentuk array 2D
 function dimensiBaris {
     for baris in $*
     do
@@ -20,5 +20,5 @@ function dimensiKolom {
     echo
 }
 
-#Memanggil funcion untuk menampilkan array 2D
+#Memanggil function untuk menampilkan array 2D
 dimensiBaris $array2d
